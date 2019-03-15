47de38113fb7d62cb1c33a32bfad9402e0004ce9
Alexander <gasfull98@gmail.com>
2019-03-15 18:39:18 +0300

    Fix php exception and add reference for getter builder.

--- src/main/java/ru/uniteller/plugin/providers/type/DtoBuilderTypeProvider.java
+++ src/main/java/ru/uniteller/plugin/providers/type/DtoBuilderTypeProvider.java
@@ -2,10 +2,14 @@ package ru.uniteller.plugin.providers.type;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.PhpClass;
 import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
 import com.jetbrains.php.lang.psi.resolve.types.PhpTypeProvider3;
 import org.jetbrains.annotations.Nullable;
+import ru.uniteller.plugin.resolver.reference.MagicMethodDtpBuilderReferenceResolver;
 
 import java.util.Collection;
 import java.util.Set;
@@ -13,16 +17,54 @@ import java.util.Set;
 public class DtoBuilderTypeProvider implements PhpTypeProvider3 {
     @Override
     public char getKey() {
-        return 0;
+        return 'D';
     }
 
     @Nullable
     @Override
     public PhpType getType(PsiElement psiElement) {
+
+        if (psiElement instanceof MethodReference && isMagicMethodDtoBuilder((MethodReference) psiElement)) {
+
+            PhpClass phpClass = this.getPhpClassByParameterMagicMethodDtoBuilder((MethodReference) psiElement);
+            if (phpClass == null) {
+                return null;
+            }
+            return PhpType.builder().add(phpClass.getFQN() + "Builder").build();
+        }
+
         return null;
     }
 
+    /**
+     * Detect magic method dto builder
+     *
+     * @param methodReference method reference
+     * @return {@code true} is magic method dto builder else {@code false}
+     */
+    private boolean isMagicMethodDtoBuilder(MethodReference methodReference) {
+        return methodReference.getSignature().equals(MagicMethodDtpBuilderReferenceResolver.SIGNATURE_METHOD_CREATE) && methodReference.getParameters().length == 1;
+    }
+
+    /**
+     * Get php class by parameter of magic method dto builder
+     *
+     * @param methodReference method reference
+     * @return PhpClass instance if can't found return {@code null}
+     */
+    @Nullable
+    private PhpClass getPhpClassByParameterMagicMethodDtoBuilder(MethodReference methodReference) {
+        PhpIndex phpIndex = PhpIndex.getInstance(methodReference.getProject());
+        String FQN = methodReference.getParameters()[0].getText().replace("::class", "");
+        for (PhpClass phpClass : phpIndex.getClassesByFQN(FQN)) {
+            return phpClass;
+        }
+        return null;
+    }
+
+
     @Override
+
     public Collection<? extends PhpNamedElement> getBySignature(String s, Set<String> set, int i, Project project) {
         return null;
     }
--- src/main/java/ru/uniteller/plugin/resolver/reference/MagicMethodDtpBuilderReferenceResolver.java
+++ src/main/java/ru/uniteller/plugin/resolver/reference/MagicMethodDtpBuilderReferenceResolver.java
@@ -1,8 +1,7 @@
 package ru.uniteller.plugin.resolver.reference;
 
-import com.jetbrains.php.lang.psi.elements.MethodReference;
-import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
-import com.jetbrains.php.lang.psi.elements.PhpReference;
+import com.jetbrains.php.PhpIndex;
+import com.jetbrains.php.lang.psi.elements.*;
 import com.jetbrains.php.lang.psi.resolve.PhpReferenceResolver;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,16 +13,41 @@ import java.util.List;
  * Reference magic method for DtoBuilder
  */
 public class MagicMethodDtpBuilderReferenceResolver implements PhpReferenceResolver {
+
+    /**
+     * Signature for method create at DTO Builder
+     * TODO replace this to settings
+     */
+    public static final String SIGNATURE_METHOD_CREATE = "#M#C\\App\\Library\\DtoBuilder\\DtoBuilder.create";
+
     @Override
     @Nullable
     public Collection<? extends PhpNamedElement> resolve(PhpReference phpReference) {
 
-        if (phpReference instanceof MethodReference){
-            List<PhpNamedElement> result = new ArrayList<>();
-
-
-            return result;
+        if (phpReference instanceof MethodReference) {
+            MethodReference methodReference = (MethodReference) phpReference;
+            PhpExpression phpExpression = methodReference.getClassReference();
+            if (phpExpression != null && phpExpression.getDeclaredType().toString().contains(SIGNATURE_METHOD_CREATE)) {
+                return this.getReferenceForMagicMethodBuilder(methodReference);
+            }
         }
         return null;
+    }
+
+    private List<PhpNamedElement> getReferenceForMagicMethodBuilder(MethodReference methodReference) {
+        List<PhpNamedElement> phpNamedElements = new ArrayList<>();
+
+        String methodName = methodReference.getName().replace("get", "").replace("set", "").replace("has", "");
+
+        methodName = Character.toLowerCase(methodName.charAt(0)) + methodName.substring(1);
+        String nameOfDto = ((MethodReference) methodReference.getClassReference()).getParameters()[0].getText().replace("::class", "");
+        PhpIndex phpIndex = PhpIndex.getInstance(methodReference.getProject());
+        PhpClass phpClass = (PhpClass) phpIndex.getClassesByFQN(nameOfDto).toArray()[0];
+        for (Field field : phpClass.getFields()) {
+            if (field.getName().equals(methodName)) {
+                phpNamedElements.add(field);
+            }
+        }
+        return phpNamedElements;
     }
 }
