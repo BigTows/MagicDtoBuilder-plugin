3577990311643e16e344a4e8475adbda47d152d9
Alexander <gasfull98@gmail.com>
2019-03-20 20:09:54 +0300

    move plugin to another package.

--- src/main/java/io/github/bigtows/plugin/magicdtobuilder/inspection/local/fix/AppendDtoClassIntoPhpDocParamTagQuickFix.java
+++ src/main/java/io/github/bigtows/plugin/magicdtobuilder/inspection/local/fix/AppendDtoClassIntoPhpDocParamTagQuickFix.java
@@ -8,7 +8,7 @@
  * Licensed under the MIT License. See LICENSE file in the project root for license information.
  */
 
-package ru.uniteller.plugin.magicdtobuilder.inspection.local.fix;
+package io.github.bigtows.plugin.magicdtobuilder.inspection.local.fix;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
